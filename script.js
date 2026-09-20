dfdf1dcb6be066b88b9ea93497f0deb0b064c170
iscariotmanagement <iscariotmanagementprofile@gmail.com>
2026-09-20 15:15:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-document.addEventListener("mousemove", function (event) {
-    document.body.style.backgroundColor =
-        `rgb(${Math.floor(event.clientX / window.innerWidth * 80)}, 0, 0)`;
+const scene = document.querySelector(".space");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let currentX = 0;
+let currentY = 0;
+
+window.addEventListener("mousemove", (e) => {
+    mouseX = (e.clientX / window.innerWidth - 0.5) * 2;
+    mouseY = (e.clientY / window.innerHeight - 0.5) * 2;
 });
+
+function animate() {
+    // smooth movement
+    currentX += (mouseX - currentX) * 0.05;
+    currentY += (mouseY - currentY) * 0.05;
+
+    // rotate the WHOLE environment
+    scene.style.transform =
+        `rotateX(${currentY * -5}deg)
+         rotateY(${currentX * 8}deg)`;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
